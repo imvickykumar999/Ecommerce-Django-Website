52c34c6ec0c47ee5e359053040966a0cc31cc1e3
Vicky Kumar <imvickykumar999@gmail.com>
2026-03-02 16:42:16 +0530

    Register New Users - Django Wednesdays ECommerce #8

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 def home(request):
     products = Product.objects.all()
@@ -29,3 +30,26 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Logged out successfully.")
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password != confirm_password:
+            messages.error(request, "Passwords do not match.")
+            return redirect('register')
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "Username already exists.")
+            return redirect('register')
+
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        messages.success(request, "Account created successfully.")
+
+        login(request, user)
+        return redirect('home')
+    else:
+        return render(request, 'register.html', {})
