010f927846a62ffeb71c458b9982eec6ffdd5314
Vicky Kumar <imvickykumar999@gmail.com>
2026-03-03 16:07:43 +0530

    Shopping Cart Setup - Django Wednesdays ECommerce #11

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +65,7 @@ JAZZMIN_SETTINGS = {
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
